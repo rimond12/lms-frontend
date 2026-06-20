34fae5c11ca76cc0032e0e7232f1d86e8317a687
Your Namerimond12 <rimondey010@gmail.com>
2026-06-20 11:54:17 +0600

    deploy issue check

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin(
   './src/i18n/request.ts'
 );
- 
+
 const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
   images: {
     unoptimized: true,
+
     remotePatterns: [
       {
         protocol: "https",
@@ -33,5 +34,5 @@ const nextConfig: NextConfig = {
     ],
   },
 };
- 
+
 export default withNextIntl(nextConfig);
